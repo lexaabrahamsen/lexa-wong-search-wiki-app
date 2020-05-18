741e7ffbaef106804e2d3e02d11f06a10fe52c28
Lexa <lexaraea@gmail.com>
2020-05-18 19:19:35 -0400

    Update search API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      wikiSearchTerms: '',
+    }
+  }
+
+  useWikiSearchEngine = () => {
+
+  }
+
+  changeWikiSearchTerms = (e) => {
+    this.setState({
+      WikiSearchTerms: e.target.value
+    });
+  }
+
+  render() {
+    let wikiSearchResults = [];
+
+    return (
+      <div className="App">
+        <h1>Hi Roon Labs! What's on your mind?</h1>
+        <form action="">
+          <input type="text" value={this.state.WikiSearchTerms} onChange={this.changeWikiSearchTerms} placeholder="Search..." />
+          <button type="submit" onClick={this.useWikiSearchEngine}>Search</button>
+        </form>
+        {wikiSearchResults}
+      </div>
+    );
+  }
 }
 
 export default App;
