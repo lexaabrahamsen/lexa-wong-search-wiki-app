3c00540796dbebbfb4c3ddbf7c36401ab37ca856
Lexa <lexaraea@gmail.com>
2020-05-18 20:27:41 -0400

    Add wikipedia api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,81 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      wikiSearchTerms: '',
+      wikiSearchReturnValues: [],
+      WikiSearchTerms: ''
     }
   }
 
-  useWikiSearchEngine = () => {
+  useWikiSearchEngine = (e) => {
+    e.preventDefault();
 
+    this.setState({
+      wikiSearchReturnValues: []
+    });
+
+    const pointerToThis = this;
+
+    var url = "http://en.wikipedia.org/w/api.php";
+
+    var params = {
+      action: 'query',
+      list: 'search',
+      srsearch: this.state.WikiSearchTerms,
+      format: 'json'
+    };
+
+    url = url + '?origin=*';
+    Object.keys(params).forEach((key) => {
+      url += "&" + key + "=" + params[key];
+    });
+
+    fetch(url)
+      .then(
+        function (response) {
+          return response.json();
+        }
+      )
+      .then(
+        function (response) {
+          // console.log(response.query.search[0].pageid);
+
+          for (var key in response.query.search) {
+            pointerToThis.state.wikiSearchReturnValues.push({
+              queryResultPageFullURL: 'no link',
+              queryResultPageID: response.query.search[key].pageid,
+              queryResultPageTitle: response.query.search[key].title,
+              queryResultPageSnippet: response.query.search[key].snipet
+            });
+          }
+        }
+      )
+      .then(
+        function (response) {
+          for (var key2 in pointerToThis.state.wikiSearchReturnValues) {
+            let page = pointerToThis.state.wikiSearchReturnValues[key2];
+            let pageID = page.queryResultPageID;
+            let urlForRetrievingPageURLByPageID = `https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=info&pageids=${pageID}&inprop=url&format=json`;
+
+            fetch(urlForRetrievingPageURLByPageID)
+              .then(
+                function (response) {
+                  return response.json();
+                }
+              )
+              .then(
+                function (response) {
+                  page.queryResultPageFullURL = response.query.pages[pageID].fullurl;
+
+                  pointerToThis.forceUpdate();
+                }
+              )
+          }
+        }
+      )
   }
 
   changeWikiSearchTerms = (e) => {
@@ -26,7 +91,7 @@ class App extends React.Component {
       <div className="App">
         <h1>Hi Roon Labs! What's on your mind?</h1>
         <form action="">
-          <input type="text" value={this.state.WikiSearchTerms} onChange={this.changeWikiSearchTerms} placeholder="Search..." />
+          <input type="text" value={this.state.WikiSearchTerms || ''} onChange={this.changeWikiSearchTerms} placeholder="Search..." />
           <button type="submit" onClick={this.useWikiSearchEngine}>Search</button>
         </form>
         {wikiSearchResults}
